f835c402047f5c88dff33dd8c55f5c2c71a777e3
MaxKutz03 <steelysundew@gmail.com>
2018-06-29 13:07:32 -0400

    Final
    Pushing your final class version! - Mirek

---
+++ Assets/HitPLayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPLayer : MonoBehaviour
+{
+    public int AttackDamage;
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        HUD_Manager hudmanager = other.gameObject.GetComponent<HUD_Manager>();
+        if (hudmanager != null)
+
+        {
+            hudmanager.UpdateHealth(AttackDamage);
+        }
+
+    }
+}
---
+++ Assets/sword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword : MonoBehaviour
+{
+    public int AttackDamage;
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        EnemyStats EnemyStats = other.gameObject.GetComponent<EnemyStats>();
+        if (EnemyStats != null)
+
+        {
+            EnemyStats.UpdateHealth(AttackDamage);
+        }
+    }
+}
+       
